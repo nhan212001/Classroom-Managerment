0149e6c7c61218d1a3b16a5f4977d342626b15e4
nhan212001 <nhan212001@yahoo.com.vn>
2025-08-16 16:18:39 +0700

    https for BE

--- BE/server.js
+++ BE/server.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const app = require('./src/app');
+const https = require('https')
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+const httpsOptions = {
+    key: fs.readFileSync('./src/config/server.key'),
+    cert: fs.readFileSync('./src/config/server.cert')
+};
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+https.createServer(httpsOptions, app).listen(port, () => {
+    console.log(`Server running at https://localhost:${port}`);
 });
